8d4a4c44a29ef5be1722443ec0447015bbf3c752
Binita Subedi <subedibinita94@gmail.com>
2020-09-16 12:44:06 +0545

    Exercise1 done

---
+++ Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main0(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
