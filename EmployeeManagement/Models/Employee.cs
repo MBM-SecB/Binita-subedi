5f3eda5d46196c6d3673a42e14a039fa52052865
Binita Subedi <subedibinita94@gmail.com>
2020-10-12 12:27:20 +0545

    Employees.Management

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -6,18 +6,18 @@ public class Employee
     public string FirstName { get; set; }
     public string Surname { get; set; }
     public string Address { get; set; }
-    public char Gender { get; set; } = 'M';
+    public char Gender { get; set; }
     public double Salary { get; set; }
 
     public static List<Employee> GetEmployees()
     {
         // Object initializer
-        var employee1 = new Employee { FirstName = "Bishnu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee2 = new Employee { FirstName = "Ram", Surname = "Basnet", Address = "Bhaktapur", Salary = 10000.0 };
-        var employee3 = new Employee { FirstName = "KP", Surname = "Oli", Address = "Ktm", Salary = 100.0 };
-        var employee4 = new Employee { FirstName = "Bishnu1", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee5 = new Employee { FirstName = "Bishnu2", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee6 = new Employee { FirstName = "Bishnu3", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
+        var employee1 = new Employee { FirstName = "Binita", Surname = "Subedi", Address = "Kathmandu", Salary = 10000.0 };
+        var employee2 = new Employee { FirstName = "Hari", Surname = "K.c", Address = "Bhaktapur", Salary = 10000.0 };
+        var employee3 = new Employee { FirstName = "Sushant", Surname = "Sharma", Address = "Thankot", Salary = 13000.0 };
+        var employee4 = new Employee { FirstName = "Saurav", Surname = "Bhusal", Address = "Lalitpur", Salary = 12000.0 };
+        var employee5 = new Employee { FirstName = "Binay", Surname = "Subedi", Address = "Ktm", Salary = 100.0 };
+        var employee6 = new Employee { FirstName = "Sabita", Surname = "Sitaula", Address = "Jhapa", Salary = 15000.0 };
 
         var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
         return employees;     
